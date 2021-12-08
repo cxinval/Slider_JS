39f85e5bedcf67973567994b7b1c9ea60608e4d3
Slider_JS <cxinval7@mail.ru>
2021-12-08 14:36:50 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-let slide = 0;
-let sliderItems = document.querySelector ('.slider-items');
 
-document.querySelector('.next').addEventListener('click',function(){  
-slide = slide + 1140;
-sliderItems.style.left = slide . px; 
+let start = 0;
+const sliderWrap = document.querySelector('.slider-wrap');
+
+document.querySelector('#next').addEventListener('click',function(){  
+    start += 717;
+    if(start > 2868){
+        start = 0;
+    }
+    sliderWrap.style.left = -start + 'px';
+    
 });
