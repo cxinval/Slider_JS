eb7298e5f47621a45370c95b61cea33f4379af48
slider <cxinval7@mail.ru>
2021-12-10 16:48:33 +0300

    10.12.2021 16:48

--- js/main.js
+++ js/main.js
@@ -1,34 +1,33 @@
-let meaning = 0;
-let next = document.querySelector(".next");
+// let meaning = 0;
+// let next = document.querySelector(".next");
 
 
-let slider = {
-    nameItem: document.querySelector(".slider-items"),
-    swipeNext: function swipe() {
-        meaning = meaning + 113.8;
-        if (meaning > 455.2) {
-            meaning = 0
-        }
-        slider.nameItem.style.left = -meaning + "em";
-    },
-    swipePrev: function swipe() {
-        meaning = meaning - 113.8;
-        if (meaning < 0) {
-            meaning = 455.2;
-        }
-        slider.nameItem.style.left = -meaning + "em";
-    },
+// let slider = {
+//     nameItem: document.querySelector(".slider-items"),
+//     swipeNext: function swipe() {
+//         meaning = meaning + 113.8;
+//         if (meaning > 455.2) {
+//             meaning = 0
+//         }
+//         slider.nameItem.style.left = -meaning + "em";
+//     },
+//     swipePrev: function swipe() {
+//         meaning = meaning - 113.8;
+//         if (meaning < 0) {
+//             meaning = 455.2;
+//         }
+//         slider.nameItem.style.left = -meaning + "em";
+//     },
 
-};
-
-function watch() {
-    slider.nameItem.classList.add('wight_5')
+// };
+let a = document.querySelectorAll('.slider-item');
+let b = document.querySelectorAll('.sleder-items')
+c = 0;
+console.log(a);
+for (let i = 0; a.length >= i; i++) {
+    c = c + 1918;
+    b.style.widht = c + "px";
 }
 
-watch();
-
-
-
-
-next.addEventListener('click', slider.swipeNext);
-prev.addEventListener('click', slider.swipePrev);
+// next.addEventListener('click', slider.swipeNext);
+// prev.addEventListener('click', slider.swipePrev);
