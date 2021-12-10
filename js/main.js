91a647a7bc06b83f3ed0ebbe1331975ef4e7d7f4
slider <cxinval7@mail.ru>
2021-12-10 14:47:24 +0300

    обновление

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
-let sliderWrap  = document.querySelectorAll(' .slider-wrap .slider-items img');
-current = 0;
-function slider (){
-    for(let i; sliderWrap.length < i; i++){
-        sliderWrap[0].classList.add('active');
-    }
+let meaning = 0;
+let next = document.querySelector(".next");
+
+
+let slider = {
+    nameItem: document.querySelector(".slider-items"),
+    swipeNext: function swipe() {
+        meaning = meaning + 113.8;
+        if (meaning > 455.2) {
+            meaning = 0
+        }
+        slider.nameItem.style.left = -meaning + "em";
+    },
+    swipePrev: function swipe() {
+        meaning = meaning - 113.8;
+        if (meaning < 0) {
+            meaning = 455.2;
+        }
+        slider.nameItem.style.left = -meaning + "em";
+    },
+
+};
+let widths = {
+    1: slider.nameItem.clientWidth = "8000px",
+    2: slider.nameItem.clientWidth = "4263px",
+    3: slider.nameItem.clientWidth = "2924px",
+    4: slider.nameItem.clientWidth = "2175px",
+    5: slider.nameItem.clientWidth = "2175px",
+    6: slider.nameItem.clientWidth = "1727px",
+};
+
+function watch() {
+    slider.nameItem.clientWidth = "4263px";
 }
 
-document.querySelector('.next').onclick = slider;
+watch();
+
+a = slider.nameItem
+console.log(a.parentNot);
+
+next.addEventListener('click', slider.swipeNext);
+prev.addEventListener('click', slider.swipePrev);
