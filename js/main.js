6f0a5951c02572bc740f14a25b374acb35b10a7c
Alexander <korinf@programist.ru>
2021-12-27 14:56:03 +0300

    подсказки (#1)

--- js/main.js
+++ js/main.js
@@ -82,49 +82,122 @@ let itemCount = sliderItem.length;
 
 // checkBtn()
 
-let RealSlider = {
-    state: {
-        position: 0,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-    },
-    option: {
-        link: {
-            sliderWrap: document.querySelector(".slider-wrap"),
-            sliderItems: document.querySelector(".slider-items"),
-            sliderItem: document.querySelectorAll(".slider-item"),
-            bulit: document.querySelectorAll(".slider-bulit"),
-            builds: document.querySelector('.slider-bulits'),
-            next: document.querySelector('.next'),
-            prev: document.querySelector('.prev'),
-        },
-        pay: {
-            itemWidth: sliderWrap.clientWidth / slidesToShow,
-            movePosition: slidesToScroll * itemWidth,
-            itemCount: sliderItem.length
-        },
-        buttons: {
-            nextArrow: this.next.addEventListener("click", function() {
-                console.log(1)
-            }),
-            nextArrow: this.prev.addEventListener("click", function() {
-                console.log(2)
-            })
-        },
-        setPosition: this.sliderItem.style.transform = `translateX(${position}px)`
+function RealSlider(elementId, options) {
+    let slider = {
+        state: {
+            currentPage: 1, // текущая страница в слайдере
+            pages: null, // всего страниц в слайдере
+            itemPerPage: 2, // количество элементов на одной странице
+            transformOffset: 0, // смещение свойства transform при слайде. @todo: я думаю это не самое удачное решение. Намного лучше чтобы мы перемещали слайды в DOM, а не меняли transform
 
+            elements: {
+                sliderWrap: null, // Контейнер обертка для слайдера
+                sliderItemsWrap: null, // Контейнер обертка для элементов слайдера
+                sliderItems: null, // // Все элементы слайдера
+                bulitsContainer: null, // Контейнер обертка для страниц слайдера
+                next: null, // Кнопка вперед
+                prev: null, // Кнопка назад
+            }
+        },
+        option: {
+            elements: {
+                sliderWrap: null, // Контейнер обертка для слайдера
+                sliderItemsWrap: null, // Контейнер обертка для элементов слайдера
+                sliderItems: null, // // Все элементы слайдера
+                bulitsContainer: null, // Контейнер обертка для страниц слайдера
+                next: null, // Кнопка вперед
+                prev: null, // Кнопка назад
+            },
+            // setPosition: this.sliderItem.style.transform = `translateX(${position}px)`
+        },
+
+        // Это наш конструктор. В нем мы хотим определить начальные настройки, дорисовать все что нужно, и повесить обработчики
+        init: function(elementId, options) {
+            // сперва надо проверить что есть элемент с id elementId.
+            this.state.sliderElement = document.querySelector(elementId);
+            if (!this.state.sliderElement) {
+                console.log('Slider initialization error. Element #' + elementId + ' not found');
+                return false;
+            }
+
+            // тут мы хотим проверить что в options указано все что нам необходимо и значения верные.
+            // то есть сохраняем наши конфиги из options в наш слайдер.
+            if (options.sliderWrap) {
+                this.state.sliderWrap = options.sliderWrap;
+            }
+            if (options.itemPerPage) {
+                this.state.itemPerPage = options.itemPerPage;
+            }
+
+            // Тут мы сохраним элементы которые нужны для дальнейшей работы
+            this.state.elements = {
+                sliderWrap: this.state.sliderElement.querySelector(".slider-wrap"),
+                sliderItemsWrap: this.state.sliderElement.querySelector(".slider-items"),
+                 sliderItems: this.state.sliderElement.querySelectorAll(".slider-item"),
+                    // bulit: document.querySelectorAll(".slider-bulit"),
+                    bulletsContainer: this.state.sliderElement.querySelector('.slider-bulits'),
+                    next: this.state.sliderElement.querySelector('.next'),
+                    prev: this.state.sliderElement.querySelector('.prev'),
+            }
+
+            // вычисляем количество в счетчике страниц
+            const itemWidth = this.state.elements.sliderItemsWrap.clientWidth / this.state.itemPerPage;
+            this.state.elements.sliderItems.forEach((item) => {
+                item.style.minWidth = itemWidth + "px"
+            })
+
+            // вычисляю смещение для перехода страниц
+            this.state.transformOffset = this.state.itemPerPage * itemWidth;
+
+            // сколько всего страниц
+            this.state.pages = this.state.elements.sliderItems.length / this.state.itemPerPage
+            // отображаю точки переключения страниц в слайдере
+            this.state.elements.bulletsContainer.innerHTML = '';
+            for (let i = 1; i <= this.state.pages; i++) {
+                let bullet = document.createElement('div')
+                bullet.setAttribute('class', 'slider-bulit ' + ((i === this.state.currentPage) ? 'active' : ''))
+                this.state.elements.bulletsContainer.append(bullet)
+            }
+
+
+            // тут мы хотим инициализировать все события.
+            // Нужно четко понимать как работает this. @see: https://learn.javascript.ru/object-methods
+            this.state.elements.next.addEventListener('click', this.next.bind(this)) // !!!! разберись зачем тут нужен bind
+            this.state.elements.prev.addEventListener('click', this.prev) // сравни что будет в prev без bind.
+
+            return this
+        },
+
+        next: function () {
+            console.log(this, this.state.sliderElement.getAttribute('id'));
+
+            if (this.state.currentPage + 1 > this.state.pages) {
+                // мы вышли за предел слайдера
+            } else {
+                let position = this.state.transformOffset * this.state.currentPage;
+                this.state.currentPage++;
+                this.state.elements.sliderItemsWrap.style.transform = `translateX(-${position}px)`;
+
+                // отображаю точки переключения страниц в слайдере
+                this.state.elements.bulletsContainer.innerHTML = '';
+                for (let i = 1; i <= this.state.pages; i++) {
+                    let bullet = document.createElement('div')
+                    bullet.setAttribute('class', 'slider-bulit ' + ((i === this.state.currentPage) ? 'active' : ''))
+                    this.state.elements.bulletsContainer.append(bullet)
+                }
+            }
+        },
+
+        prev: function () {
+            console.log(this);
+        },
     }
+
+    return slider.init(elementId, options) // можно отрефакторить покрасивее. Избавиться от вызова init метода
 }
 
-
-
-
-
-// prev.addEventListener("click", function() {
-//     position += movePosition
-//     setPosition();
-//     // checkBtn();
-// })
-// sliderItem.forEach((item) => {
-//     item.style.minWidth = itemWidth + "px"
-// })
+/**
+ * Тут мы хотим использовать два слайдера
+ */
+var slider1 = new RealSlider('#slider1', {});
+var slider2 = new RealSlider('#slider2', {itemPerPage: 3});
